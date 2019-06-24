5c0b3903aa66a1c4410842fdbaf5ee2a0e2374f7
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-06-24 22:55:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,10 @@ introduction("Marisa")
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language("Marisa", "Ruby")
+introduction_with_language("Marisa", "Javascript")
+
+def introduction_with_language_optional(name, language)
+  language = "Ruby"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language_optional("Marisa", "Javascript")
