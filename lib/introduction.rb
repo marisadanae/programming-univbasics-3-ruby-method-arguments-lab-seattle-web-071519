0d692a41d5a18df6ff29ac75dcafeeb95f937cd5
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-06-24 23:25:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,12 +10,11 @@ introduction_with_language("Marisa", "Javascript")
 
 #Takes two arguments(x, y) & default argument is set to Ruby, but still accepts a name
 
-def introduction_with_language_optional(name)
-  puts "Hi, my name is #{name} and I am learning to program in Ruby."
-end
-introduction_with_language_optional("Marisa")
-
 def introduction_with_language_optional(name, language)
+if language = "Ruby"
+  puts "Hi, my name is #{name} and I am learning to program in Ruby."
+else
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
+introduction_with_language_optional("Marisa")
 introduction_with_language_optional("Marisa", "Python" )
